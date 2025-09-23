9e90dfa55df6f6a28de49b86ed685a949b8abb47
dominik huffield <cogentdom@gmail.com>
2025-09-22 22:16:19 -0600

    ...

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,13 @@
 from agents import Agent, Runner
+import os
+import openai
+
+# For using OpenAI to generate query embedding
+from dotenv import load_dotenv
+load_dotenv()
+open_api_key = os.getenv("OPENAI_API_KEY")
+openai_client = openai.OpenAI(api_key=open_api_key)
+
 
 agent = Agent(name="assistant", instructions="You are a helpful assistant.")
 
