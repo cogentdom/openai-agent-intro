019f59a838209ca9c0e24bf48b8385a285786d85
dominik huffield <cogentdom@gmail.com>
2025-10-07 21:56:04 -0600

    ...

---
+++ agent-1.py
@@ -0,0 +1,15 @@
+from agents import Agent, ModelSettings, function_tool
+from dotenv import load_dotenv
+load_dotenv()
+
+@function_tool
+def get_weather(city: str) -> str:
+    """returns weather info for the specified city."""
+    return f"The weather in {city} is sunny"
+
+agent = Agent(
+    name="Haiku agent",
+    instructions="Always respond in haiku form",
+    model="gpt-5-nano",
+    tools=[get_weather],
+)
